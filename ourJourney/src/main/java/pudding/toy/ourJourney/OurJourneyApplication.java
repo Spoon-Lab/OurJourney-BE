a6b1b219d9b3c1288a4c29a97a65cc66d4fac9d0
chaerish <ccyy8432@naver.com>
2024-08-29 18:05:31 +0900

    ✨ feat: 프로젝트 초기 세팅

---
+++ ourJourney/src/main/java/pudding/toy/ourJourney/OurJourneyApplication.java
@@ -0,0 +1,13 @@
+package pudding.toy.ourJourney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurJourneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurJourneyApplication.class, args);
+	}
+
+}
---
+++ ourJourney/src/test/java/pudding/toy/ourJourney/OurJourneyApplicationTests.java
@@ -0,0 +1,13 @@
+package pudding.toy.ourJourney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurJourneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
