511b9aab3db5a94fa053ae7fd814c10e28d547f5
chaerish <ccyy8432@naver.com>
2024-08-31 00:24:58 +0900

    ✨ feat: profile entity + repository 생성

--- ourJourney/src/main/java/pudding/toy/ourJourney/repository/ProfileRepository.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/repository/ProfileRepository.java
@@ -1,2 +1,9 @@
-package pudding.toy.ourJourney.repository;public interface ProfileRepository {
+package pudding.toy.ourJourney.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import pudding.toy.ourJourney.entity.Profile;
+
+@Repository
+public interface ProfileRepository extends JpaRepository<Profile, Long> {
 }
