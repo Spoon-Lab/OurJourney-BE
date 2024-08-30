4f02cc09c1ffbf2f5e8ba3d88fb21db40a30faa6
chaerish <ccyy8432@naver.com>
2024-08-31 00:24:58 +0900

    ✨ feat: profile entity + repository 생성

--- ourJourney/src/main/java/pudding/toy/ourJourney/entity/Profile.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/entity/Profile.java
@@ -1,2 +1,20 @@
-package pudding.toy.ourJourney.entity;public class Profile {
+package pudding.toy.ourJourney.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity @Getter
+@NoArgsConstructor @AllArgsConstructor @Builder
+public class Profile {
+    @Id
+    private Long id;
+    private String nickName;
+    private String profileImg;
+    private String selfIntroduction;
+
 }
--- ourJourney/src/test/java/pudding/toy/ourJourney/OurJourneyApplicationTests.java
+++ ourJourney/src/test/java/pudding/toy/ourJourney/OurJourneyApplicationTests.java
@@ -8,6 +8,7 @@ class OurJourneyApplicationTests {
 
 	@Test
 	void contextLoads() {
+
 	}
 
 }
