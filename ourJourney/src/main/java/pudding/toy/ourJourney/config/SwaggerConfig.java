c7b73cb7ac5aff95803feff99f3ce1da16a0758f
rockpell <aythffk@gmail.com>
2024-09-07 02:44:11 +0900

    chore: swagger config

---
+++ ourJourney/src/main/java/pudding/toy/ourJourney/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package pudding.toy.ourJourney.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Our Journey API",
+                description = "Our Journey API Documentation",
+                version = "v1")
+)
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi openApi() {
+        String[] paths = {"/**"};
+
+        return GroupedOpenApi.builder()
+                .group("Our Journey API v1")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
