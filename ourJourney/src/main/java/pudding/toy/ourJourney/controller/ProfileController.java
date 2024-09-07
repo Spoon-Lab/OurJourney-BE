942454a8598df5283114bcf1073727a4f914c5fa
rockpell <aythffk@gmail.com>
2024-09-07 23:22:13 +0900

    docs: 프로필 조회 api

--- ourJourney/src/main/java/pudding/toy/ourJourney/controller/ProfileController.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/controller/ProfileController.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import pudding.toy.ourJourney.dto.profile.CreateProfileRequest;
+import pudding.toy.ourJourney.dto.profile.GetDetailProfileResponse;
+
+import java.util.Optional;
 
 @Tag(name = "Profile API")
 @RestController
@@ -15,4 +18,11 @@ public class ProfileController {
     @PostMapping("")
     public void createProfile(@RequestBody CreateProfileRequest body) {
     }
+
+    @Operation(summary = "프로필 조회")
+    @GetMapping("/{id}")
+    public GetDetailProfileResponse getProfile(@PathVariable Long id) {
+        return new GetDetailProfileResponse(1L, Optional.of("nickname"), null, null);
+    }
+
 }
