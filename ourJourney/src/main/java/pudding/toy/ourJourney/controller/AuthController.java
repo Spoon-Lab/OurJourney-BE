383d350e0751cf62dab9ffc378a974a16baadba9
chaerish <ccyy8432@naver.com>
2024-09-07 14:17:12 +0900

    ✨ feat: django auth 틀 생성

--- ourJourney/src/main/java/pudding/toy/ourJourney/controller/AuthController.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/controller/AuthController.java
@@ -2,11 +2,19 @@ package pudding.toy.ourJourney.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pudding.toy.ourJourney.common.response.BaseResponse;
+import pudding.toy.ourJourney.dto.auth.ProfileAuthResponseDto;
+import pudding.toy.ourJourney.service.AuthService;
 
 @RestController
 @RequiredArgsConstructor
 public class AuthController {
+    @Autowired
+    AuthService authService;
+
 
 }
