e37272f95e3f1e0ff0ab27ab4d8e9b52f41b45aa
rockpell <aythffk@gmail.com>
2024-09-07 11:14:34 +0900

    docs: 대댓글 생성 api

--- ourJourney/src/main/java/pudding/toy/ourJourney/controller/CommentController.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/controller/CommentController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import pudding.toy.ourJourney.dto.comment.CreateCommentRequest;
 import pudding.toy.ourJourney.dto.comment.CreateCommentResponse;
+import pudding.toy.ourJourney.dto.comment.CreateReCommentRequest;
+import pudding.toy.ourJourney.dto.comment.CreateReCommentResponse;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +18,20 @@ public class CommentController {
 
     @Operation(summary = "댓글 생성")
     @PostMapping("/contents/{contentsId}/comments/")
-    public CreateCommentResponse createComment(@PathVariable("contentsId") Long contentsId, @RequestBody @Valid CreateCommentRequest body) {
+    public CreateCommentResponse createComment(
+            @PathVariable("contentsId") Long contentsId,
+            @RequestBody @Valid CreateCommentRequest body
+    ) {
         return new CreateCommentResponse(1L);
     }
 
+    @Operation(summary = "대댓글 생성")
+    @PostMapping("/contents/{contentsId}/comments/{commentId}/")
+    public CreateReCommentResponse createReComment(
+            @PathVariable("contentsId") Long contentsId,
+            @PathVariable("commentId") Long commentId,
+            @RequestBody @Valid CreateReCommentRequest body
+    ) {
+        return new CreateReCommentResponse(1L);
+    }
 }
