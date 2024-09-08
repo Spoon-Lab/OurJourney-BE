71891cb8410933a1a97261a8f4afa03f954bc351
rockpell <aythffk@gmail.com>
2024-09-08 13:52:40 +0900

    docs: 해시태그 목록 api

--- ourJourney/src/main/java/pudding/toy/ourJourney/controller/TagsController.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/controller/TagsController.java
@@ -3,11 +3,15 @@ package pudding.toy.ourJourney.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
 import pudding.toy.ourJourney.dto.tags.CreateTagsRequest;
+import pudding.toy.ourJourney.dto.tags.GetTagsDto;
+import pudding.toy.ourJourney.dto.tags.GetTagsResponse;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,5 +21,12 @@ public class TagsController {
     @Operation(summary = "해시태그 생성")
     @PostMapping("")
     public void createTag(@RequestBody @Valid CreateTagsRequest createTagsRequest) {
+    }
+
+    @Operation(summary = "해시태그 가져오기", description = "해시태그 자동완성할때 호출")
+    @GetMapping("")
+    public GetTagsResponse getTags(@PageableDefault Pageable pageable, @RequestParam() String tagName) {
+        List<GetTagsDto> list = List.of(new GetTagsDto(1L, "tag1"));
+        return new GetTagsResponse(new PageImpl<>(list, pageable, 1L));
     }
 }
