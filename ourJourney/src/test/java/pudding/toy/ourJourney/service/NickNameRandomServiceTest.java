dab145f43eea1054bd6aba3ecf277ba07c6c0616
chaerish <ccyy8432@naver.com>
2024-09-12 19:26:25 +0900

    :sparkles: CreateProfile 로직 구현 + 디폴트 닉네임 설정

---
+++ ourJourney/src/test/java/pudding/toy/ourJourney/service/NickNameRandomServiceTest.java
@@ -0,0 +1,30 @@
+package pudding.toy.ourJourney.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pudding.toy.ourJourney.dto.auth.ProfileAuthResponseDto;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+@SpringBootTest
+public class NickNameRandomServiceTest {
+    @Autowired
+    ProfileService profileService;
+    @Test
+    void randomTest(){
+        ArrayList<String> randoms = new ArrayList<>(
+                Arrays.asList("예쁜", "졸린", "작은", "큰", "빠른", "따뜻한", "밝은", "산뜻한", "사랑스러운", "행복한", "귀여운")
+        );
+        int randomIndex = (int)(Math.random()*randoms.size());
+        System.out.println(randoms.get(randomIndex)+"푸딩"+(int)(Math.random()*1000)+"번");
+    }
+    @Test
+    void createProfile(){
+        ProfileAuthResponseDto profileAuthResponseDto = new ProfileAuthResponseDto(3L);
+        System.out.println(profileService.createProfile(profileAuthResponseDto).getNickName());
+
+    }
+}
