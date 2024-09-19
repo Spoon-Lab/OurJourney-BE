4ea10c28c470870eda485a9c7fa550fdad205285
chaerish <ccyy8432@naver.com>
2024-09-19 20:08:47 +0900

    :recycle: refactor: follow, unfollow api url 경로 수정

--- ourJourney/src/main/java/pudding/toy/ourJourney/controller/FollowController.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/controller/FollowController.java
@@ -14,16 +14,16 @@ import pudding.toy.ourJourney.service.ProfileService;
 @RestController
 @RequiredArgsConstructor
 @Tag(name = "Category API", description = "Category API 입니다.")
-@RequestMapping("/follow")
+@RequestMapping("/profile")
 public class FollowController {
     private final ProfileInitializer profileInitializer;
     private final FollowService followService;
-    @PostMapping("/{id}")
+    @PostMapping("/{id}/follow")
     @Operation(summary = "팔로우 하기", description = "다른 사용자를 팔로우 한다.")
     public void follow(@PathVariable("id") Long id){
         followService.followProfile(profileInitializer.dummyProfile,id);
     }
-    @PostMapping("/unfollow/{id}")
+    @PostMapping("/{id}/unfollow")
     @Operation(summary = "팔로우 취소하기", description = "다른 사용자를 언팔로우한다.")
     public void unFollow(@PathVariable("id") Long id){
         followService.unFollow(profileInitializer.dummyProfile,id);
