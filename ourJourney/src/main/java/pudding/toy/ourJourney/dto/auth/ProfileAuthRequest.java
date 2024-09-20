74343661b3edf28d757f03269f46bec1a9d4287c
chaerish <ccyy8432@naver.com>
2024-09-20 17:37:48 +0900

    :bug: JsonProperty 제거

--- ourJourney/src/main/java/pudding/toy/ourJourney/dto/auth/ProfileAuthRequest.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/dto/auth/ProfileAuthRequest.java
@@ -7,6 +7,5 @@ import lombok.NoArgsConstructor;
 
 @Data @NoArgsConstructor @AllArgsConstructor
 public class ProfileAuthRequest { //django 가 보내주는 요청에 대한 응답이 유저 pk
-    @JsonProperty("user_id")
     Long id; //user pk
 }
