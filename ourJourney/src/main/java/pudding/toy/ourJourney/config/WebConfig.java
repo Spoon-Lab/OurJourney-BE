6e2b662cad9e048102aa6cf835d96ac4ce0da40f
rockpell <aythffk@gmail.com>
2024-09-20 12:38:32 +0900

    feat: cors 설정 추가

---
+++ ourJourney/src/main/java/pudding/toy/ourJourney/config/WebConfig.java
@@ -0,0 +1,16 @@
+package pudding.toy.ourJourney.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "https://our-journey-fe.vercel.app")
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
+}
