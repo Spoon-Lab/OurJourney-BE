d7234c15e9ec2e62b409e84856086007dd3ab0f8
rockpell <aythffk@gmail.com>
2024-09-22 00:51:16 +0900

    fix: createdAt, updatedAt이 자동으로 안채워지는 경우가 있어서 추가

--- ourJourney/src/main/java/pudding/toy/ourJourney/entity/BaseTimeEntity.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/entity/BaseTimeEntity.java
@@ -11,9 +11,9 @@ import java.time.LocalDateTime;
 @MappedSuperclass @Getter
 public abstract class BaseTimeEntity {
     @CreatedDate @Column(name ="created_at")
-    private LocalDateTime createdAt; // 생성 시간
+    private LocalDateTime createdAt = LocalDateTime.now(); // 생성 시간
     @LastModifiedDate @Column(name = "updated_at")
-    private LocalDateTime updateAt; //마지막 수정 시간
+    private LocalDateTime updateAt = LocalDateTime.now(); //마지막 수정 시간
     @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
