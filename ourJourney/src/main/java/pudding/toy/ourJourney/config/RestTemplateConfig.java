b355ce5ca347117ed47937be901a7df9def2c810
rockpell <aythffk@gmail.com>
2024-09-21 15:14:31 +0900

    feat: 장고에 auth 요청하는 함수 추가

---
+++ ourJourney/src/main/java/pudding/toy/ourJourney/config/RestTemplateConfig.java
@@ -0,0 +1,18 @@
+package pudding.toy.ourJourney.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+    @Value("${spring.auth-server.url}")
+    private String authServerUrl;
+
+    @Bean
+    public RestTemplate authRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.rootUri(authServerUrl).build();
+    }
+}
--- ourJourney/src/main/java/pudding/toy/ourJourney/service/AuthService.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/service/AuthService.java
@@ -2,21 +2,38 @@ package pudding.toy.ourJourney.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+import pudding.toy.ourJourney.dto.auth.AuthResponse;
 import pudding.toy.ourJourney.repository.ProfileRepository;
+
+import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class AuthService { //django to auth
     private final ProfileRepository profileRepository;
-    public Boolean validateAuth(String authorizationHeader){
+    private final RestTemplate authRestTemplate;
+
+    public Boolean validateAuth(String authorizationHeader) {
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             String accessToken = authorizationHeader.substring(7); // "Bearer " 이후의 토큰 부분을 추출
         }
         //todo: auth
         return true;
+    }
+
+    public AuthResponse getAuth(String accessToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        HttpEntity<Object> entity = new HttpEntity<>(headers);
+        ResponseEntity<AuthResponse> response = authRestTemplate.exchange("/auth/certificate", HttpMethod.GET, entity, AuthResponse.class);
+        return response.getBody();
     }
 
 }
