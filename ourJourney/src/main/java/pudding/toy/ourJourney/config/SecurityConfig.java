c1f26275819d9e3e821d8e4b5ea84ebec679c863
rockpell <aythffk@gmail.com>
2024-09-22 22:24:18 +0900

    fix: JwtAuthenticationFilter 생성 안되는 문제 수정

--- ourJourney/src/main/java/pudding/toy/ourJourney/config/SecurityConfig.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/config/SecurityConfig.java
@@ -10,27 +10,30 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import pudding.toy.ourJourney.filter.JwtAuthenticationFilter;
+import pudding.toy.ourJourney.service.AuthService;
+import pudding.toy.ourJourney.service.CustomUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthService authService;
+    private final CustomUserDetailService userDetailService;
+
+    JwtAuthenticationFilter jwtAuthenticationFilter(AuthService authService, CustomUserDetailService userDetailService) {
+        return new JwtAuthenticationFilter(authService, userDetailService);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.GET, "/{contentId}/**").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/{contentId}/**").authenticated()
-                        .requestMatchers(HttpMethod.PATCH, "/{contentId}/**").authenticated()
-                        .requestMatchers(HttpMethod.DELETE, "/{contentId}/**").authenticated()
-                        .anyRequest().authenticated());
-        http
-                .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        http
-                .addFilterBefore(jwtAuthenticationFilter,UsernamePasswordAuthenticationFilter.class);
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/{contentId}/**").permitAll()
+                .requestMatchers(HttpMethod.POST, "/{contentId}/**").authenticated()
+                .requestMatchers(HttpMethod.PATCH, "/{contentId}/**").authenticated()
+                .requestMatchers(HttpMethod.DELETE, "/{contentId}/**").authenticated()
+                .anyRequest().authenticated());
+        http.sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(jwtAuthenticationFilter(authService, userDetailService), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
-
 }
