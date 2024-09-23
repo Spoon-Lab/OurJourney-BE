4070bd64a29c5cb3409cebf4b86c13cf20240648
chaerish <ccyy8432@naver.com>
2024-09-24 01:28:02 +0900

    :bug: fix: bug 수정

---
+++ ourJourney/src/main/java/pudding/toy/ourJourney/entity/CustomUserDetail.java
@@ -0,0 +1,33 @@
+package pudding.toy.ourJourney.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CustomUserDetail implements UserDetails {
+    private final Profile profile;
+
+    public CustomUserDetail(Profile profile) {
+        this.profile = profile;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        ArrayList<GrantedAuthority> auth = new ArrayList<GrantedAuthority>();
+        auth.add(new SimpleGrantedAuthority(Role.ROLE_USER.getRole()));
+        return auth;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return Long.toString(profile.getId());
+    }
+}
