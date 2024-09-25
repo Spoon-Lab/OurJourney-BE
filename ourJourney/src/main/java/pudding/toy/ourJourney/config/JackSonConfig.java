1f812cd035a89b7cae3420a8575d76c68d25b007
rockpell <aythffk@gmail.com>
2024-09-25 21:53:11 +0900

    fix: localDateTime json 으로 내려갈때 배열로 내려가는 문제 수정

--- ourJourney/src/main/java/pudding/toy/ourJourney/config/JackSonConfig.java
+++ ourJourney/src/main/java/pudding/toy/ourJourney/config/JackSonConfig.java
@@ -1,10 +1,13 @@
 package pudding.toy.ourJourney.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class JackSonConfig {
@@ -12,7 +15,8 @@ public class JackSonConfig {
     Jackson2ObjectMapperBuilder objectMapperBuilder(){
         Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
         builder.serializationInclusion(JsonInclude.Include.ALWAYS)
-                .modulesToInstall(new JsonNullableModule());
+                .modulesToInstall(new JsonNullableModule())
+                .serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return builder;
     }
 
